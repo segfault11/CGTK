4d7c18ef78f87cc15fe1e30574fb760397331193
thisisscatterbrain <arno.luebke@googlemail.com>
2013-10-28 23:36:17 +0900

    Test

--- src/APP/GUIElement.h
+++ src/APP/GUIElement.h
@@ -6,18 +6,13 @@
 #ifndef GUIRENDERER_H__
 #define GUIRENDERER_H__
 
-#include "../GL/Program.h"
-#include "../GL/VertexArray.h"
-#include "../GL/BufferObject.h"
 #include "../Math/Vector3.h"
-#include "TextureAtlas.h" 
 #include "IDrawable.h"
 
 namespace APP 
 {
     class GUIElement : public IDrawable
     {
-
     public:
         GUIElement(
             const std::string& text, 
@@ -31,20 +26,7 @@ namespace APP
         virtual void Draw();
 
     private:
-        DECL_DEFAULTS(GUIElement)
-        
-        void refillBufferObject();
-        void createVertexArray();
-
-        GL::VertexArray* vertexArray_;
-        GL::BufferObject* bufferObject_;
-
-        const TextureAtlas* atlas_;
-        GL::Program program_;
-
-        std::string text_;
-        Math::Vector2I position_; 
-        Math::Vector3F textColor_;        
+        DECL_DEFAULTS(GUIElement)  
     };
     
 }
